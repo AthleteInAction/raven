e6bd47873956dec4c1187260b781726e715ff58e
Will Robinson <wrobinson-web-contractor@evernote.com>
2014-05-08 13:08:20 -0700

    NEW DATES

--- app/assets/javascripts/ctrl_cal.js
+++ app/assets/javascripts/ctrl_cal.js
@@ -5,23 +5,37 @@ var CalCtrl = ['$scope','$routeParams','$location','$route','ApiModel','$timeout
 
 		$scope.months = months;
 		$scope.today = new Date();
-		$scope.month = ($scope.today.getUTCMonth()+1);
 
-		JP($scope.month);
-		JP('month_'+$scope.month);
+		if ($scope.params.month && $scope.params.month <=12 && $scope.params.month > 0){
+			$scope.month = ($scope.params.month-1);
+		} else {
+			$scope.month = $scope.today.getUTCMonth();
+		}
 
-		//$location.hash('tester');
-		//$anchorScroll();
+		if ($scope.params.year){
+			$scope.year = $scope.params.year;
+		} else {
+			$scope.year = $scope.today.getFullYear();
+		}
+
+		$scope.mtab = {};
+		$scope.mtab[$scope.month] = 'selected';
 
 		$timeout(function(){
-			$('.tcontent').snapscroll({
+			/*$('.menu').snapscroll({
 				botPadding: 1,
 				topPadding: 1,
 				scrollSpeed: 200,
 				scrollEndSpeed: 50,
-				scrollOptions: {'axis':'xy'}
+				scrollOptions: {'axis':'yx'}
 			});
-		},1000);
+			
+			$location.hash('month_4');
+			$anchorScroll();*/
+
+			$('#month_'+$scope.month).goTo();
+
+		},10);
 
 	}
 ];
