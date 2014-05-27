640eb4139e454a9b3e7f8fb6bdce16f01a45bd74
Will Robinson <wrobinson-web-contractor@evernote.com>
2014-05-26 22:40:26 -0700

    UPGRADE TO RAILS 4.1.1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Raven::Application.config.secret_key_base = '7e5367e9a51f8e9702fb5595ee49627ab50fa6d0bcfc263c2519abfe4a2af76c69c06f592b6935073fceae2f043767a6d029d284ffd77602e0e9e0caed30cb6a'
+Raven::Application.config.secret_key_base = '2e8941111d0103b6ac30abab93e5805c49f1ba9ef94c52da820d3d62ccc31c909b3cf608a8f5aeedf6043f5041b2d0a3d9bf0d52799e7eea45f0a0b84665722e'
